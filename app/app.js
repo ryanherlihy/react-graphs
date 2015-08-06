2a03c3d559ff44ae91e87dc667b573030f22e5cb
Ryan Herlihy <ryan.herlihy@sonian.net>
2015-08-06 15:04:24 -0400

    initial commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import App from './components/App';
+
+React.render(
+  <App />,
+  document.getElementById('example')
+);
---
+++ app/components/App.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+
+const {
+  Component
+} = React;
+
+class App extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default App;
